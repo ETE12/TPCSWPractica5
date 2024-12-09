48718d0bd9458f2c10fe1cda08dd6a5b258b091b
ETE12 <emilianotoes12@gmail.com>
2024-12-09 01:10:08 -0600

    subir practica 5

---
+++ src/main/java/org/uv/tpcsw/demo/TPCSWPractica5/Ejemplo.java
@@ -0,0 +1,15 @@
+
+package org.uv.tpcsw.demo.TPCSWPractica5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/url")
+public class Ejemplo {
+    @GetMapping
+    public String sayHello(){
+        return "Hola mundo!";
+    }
+}
